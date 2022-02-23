ae963c50da394c5c52e42023d535785a3d8b8599
Alexandre Lam <alexandrelam@outlook.com>
2022-02-23 11:34:16 +0100

    feat: added nested ressources

--- api/src/controllers/GenreController.ts
+++ api/src/controllers/GenreController.ts
@@ -1,12 +1,17 @@
 import { Context } from 'koa';
 import Router from 'koa-router';
 import { Controller } from '.';
-import { Genre, GenreModel } from 'nivclones-ilog-models';
+import { Genre, GenreModel, BookModel } from 'nivclones-ilog-models';
 
 export const GenreController: Controller = (router: Router) => {
   router.get('/genres', async (ctx: Context) => {
     const payload = await GenreModel.find();
     ctx.body = payload;
+  });
+
+  router.get('/books/:id/genres', async (ctx: Context) => {
+    const id: string = ctx.params.id;
+    ctx.body = await BookModel.findById(id, 'genres');
   });
 
   router.post('/genres', async (ctx: Context) => {
